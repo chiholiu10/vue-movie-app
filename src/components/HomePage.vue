9093ba095b578f49be1773d83d19be406cdfc57f
Chiho <chiho@Chihos-MacBook-Pro.local>
2022-12-03 13:40:30 +0100

    added component genrecheckboxes homepage

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,3 +1,55 @@
-<script setup></script>
+<script setup>
+import GenreCheckboxes from "./GenreCheckboxes.vue";
+import { ref, computed } from "vue";
 
-<template>HomePage</template>
+const selectedGenres = ref([]);
+const fullList = ref([]);
+
+fetch("https://api.tvmaze.com/shows")
+  .then((response) => response.json())
+  .then((response) => {
+    fullList.value = response;
+  });
+
+const genres = computed(() => {
+  const uniqueGenres = new Set();
+  fullList.value.forEach((item) => {
+    if (!item.genres) return;
+    item.genres.forEach((genre) => uniqueGenres.add(genre));
+  });
+  return [...uniqueGenres];
+});
+
+const filteredList = computed(() => {
+  if (
+    !selectedGenres.value.length &&
+    !selectedRating.value &&
+    !searchTitle.value.length &&
+    !selectedLanguage.length
+  )
+    return fullList.value;
+  return fullList.value.filter((item) => {
+    if (!selectedGenres.value.length) return true;
+    let isInGenres = false;
+    for (let genre of item.genres) {
+      isInGenres = selectedGenres.value.includes(genre);
+      if (isInGenres) {
+        break;
+      }
+    }
+    return isInGenres;
+  });
+});
+</script>
+
+<template>
+  <div class="main-container">
+    <GenreCheckboxes :genres="genres" v-model:selectedGenres="selectedGenres" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.main-container {
+  width: 100%;
+}
+</style>
