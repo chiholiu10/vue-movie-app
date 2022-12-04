5c0964755ce6ee4e76615765ec9953d2edbc52c5
Chiho <chiho@Chihos-MacBook-Pro.local>
2022-12-04 01:13:04 +0100

    no result page created

---
+++ src/components/NoResult.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="no-result-container">No movie found</div>
+</template>
+
+<style lang="scss" scoped>
+.no-result-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100px;
+}
+</style>
