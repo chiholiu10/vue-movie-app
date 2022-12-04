d4346f5de3927d45ee07762a2f190f01b71aa8d2
Chiho <chiho@Chihos-MacBook-Pro.local>
2022-12-04 04:04:33 +0100

    update styling

--- src/components/TvResult.vue
+++ src/components/TvResult.vue
@@ -1,4 +1,6 @@
 <script setup>
+import NoResult from "./NoResult.vue";
+import vue3StarRatings from "vue3-star-ratings";
 defineProps({
   filteredList: {
     type: Array,
@@ -12,62 +14,76 @@ defineProps({
 
 <template>
   <div class="scrolling-wrapper">
-    <h2>{{ headingTitle }}</h2>
-    <ul>
-      <li v-for="tv in filteredList" :key="tv.id">
-        <router-link :to="`/show/${tv.id}`" className="tv-list-anchor">
-          <img :src="tv.image?.medium" />
+    <div v-if="filteredList.length > 0">
+      <h2 class="heading-title">{{ headingTitle }}</h2>
+      <ul>
+        <li v-for="tv in filteredList" :key="tv.id">
+          <router-link :to="`/show/${tv.id}`" className="tv-list-anchor">
+            <img :src="tv.image?.medium" />
 
-          <div>
             <h2>{{ tv.name }}</h2>
-          </div>
-          <vue3-star-ratings
-            :numberOfStars="Math.floor(tv.rating.average / 2)"
-            inactiveColor="#ff9800"
-            :showControl="false"
-            :disableClick="true"
-            starSize="20"
-          />
-        </router-link>
-      </li>
-    </ul>
+            <vue3-star-ratings
+              :numberOfStars="Math.floor(tv.rating.average / 2)"
+              inactiveColor="#ff9800"
+              :showControl="false"
+              :disableClick="true"
+              starSize="20"
+            />
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <NoResult />
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .scrolling-wrapper {
   box-sizing: border-box;
-  padding: 20px;
   width: 100vw;
-}
-ul {
-  list-style-type: none;
-  overflow-x: scroll;
-  overflow-y: hidden;
-  white-space: nowrap;
-  li {
-    display: inline-block;
-    flex-direction: column;
-    min-width: 200px;
-    margin: 0 20px;
-    position: relative;
-    img {
-      max-width: 180px;
-      width: 100%;
-    }
+  .heading-title {
+    font-size: 24px;
+    font-weight: 600;
+    background-color: $transparent;
+    margin: 50px 0;
   }
-}
-li {
-  h2 {
-    font-size: 18px;
-    word-break: break-word;
-  }
-  .tv-list-anchor {
-    background-color: $black;
-    color: $white;
-    display: block;
-    &:hover {
-      color: $white;
+  ul {
+    list-style-type: none;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    white-space: nowrap;
+    li {
+      display: inline-block;
+      flex-direction: column;
+      min-width: 200px;
+      margin: 0 20px;
+      position: relative;
+      width: 200px;
+      overflow: hidden;
+      h2 {
+        font-size: 18px;
+        width: 100%;
+        overflow: hidden;
+        white-space: no-wrap;
+        text-overflow: ellipsis;
+        text-align: center;
+        padding: 10px 0;
+        opacity: 0.8;
+      }
+
+      .tv-list-anchor {
+        background-color: $black;
+        color: $white;
+        display: block;
+        &:hover {
+          color: $white;
+        }
+      }
+      img {
+        width: 100%;
+      }
     }
   }
 }
